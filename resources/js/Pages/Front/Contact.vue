311e0f41655b340deeef6dd3e5cf113354c23289
jdawkins <junior.dawkins@outlook.com>
2023-07-08 08:27:35 -0500

    fix

--- resources/js/Pages/Front/Contact.vue
+++ resources/js/Pages/Front/Contact.vue
@@ -8,12 +8,18 @@ import AppLayout from "@/Layouts/AppLayout.vue";
     <Head title="Contact Us" />
 
     <AppLayout>
-        <div class="py-12">
-            <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg h-[60vh]">
-                    <div class="p-6 text-gray-900">Contact Us</div>
+        <div class="bg-primary0 relative">
+            <img :src="asset('assets/img/hero_1.png')" alt="" class="absolute top-0 left-0">
+
+            <div class="mx-auto max-w-7xl px-6 py-16 sm:py-32 text-center">
+                <div class="uppercase py-1 leading-6 text-primary font-erica-one text-3xl">
+                    Contact Us
                 </div>
+                <p class="text-primary mt-6">If you have a question fill the form below
+                    or send us an email to <a href="mailto:support@hatwizard.com">support@hatwizard.com</a></p>
             </div>
+        </div>
+        <div class="bg-white min-h-[20vh]">
         </div>
     </AppLayout>
 </template>
--- resources/js/Pages/Front/Policy.vue
+++ resources/js/Pages/Front/Policy.vue
@@ -8,12 +8,16 @@ import AppLayout from "@/Layouts/AppLayout.vue";
     <Head title="Policy" />
 
     <AppLayout>
-        <div class="py-12">
-            <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg h-[60vh]">
-                    <div class="p-6 text-gray-900">Policy</div>
+        <div class="bg-primary0 relative">
+            <img :src="asset('assets/img/hero_1.png')" alt="" class="absolute top-0 left-0">
+
+            <div class="mx-auto max-w-7xl px-6 py-16 sm:py-32 text-center">
+                <div class="uppercase py-1 leading-6 text-primary font-erica-one text-3xl">
+                    Privacy Policy
                 </div>
             </div>
+        </div>
+        <div class="bg-white min-h-[20vh]">
         </div>
     </AppLayout>
 </template>
